1957fae13693ae12542d930aefcfcdf369d86e4b
iamvinxz <john.khervin13@gmail.com>
2025-07-24 14:22:02 +0800

    Google Chrome Extension

--- Extension/extension.js
+++ Extension/extension.js
@@ -1,20 +1,47 @@
+let myLeads = [] ;
+
 const input = document.getElementById('input-el');
-const saveBtn =  document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el');
-let myLead = [];
 
+const leadsFromStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-saveBtn.addEventListener("click", function(){
-    myLead.push(input.value);
-    input.value = "";
-    renderLeads();
-})
+//checks if local storage is empty, then if not render the leads even if the tab is closed
+if(leadsFromStorage){
+    myLeads = leadsFromStorage;
+   renderLeads(myLeads);
+}
 
-function renderLeads(){
+//render the leads into lists
+function renderLeads(leads){
     let listItems = "";
 
-    for(let i = 0; i < myLead.length; i++){
-            listItems += `<li><a target='_blank' href='${myLead[i]}'>${myLead[i]}</a></li>`;
+    for(let i = 0; i < leads.length; i++){
+            listItems += `<li><a target='_blank' href='${leads[i]}'>${leads[i]}</a></li>`;
     }
     ulEl.innerHTML = listItems;
 }
+
+//clears the array, local storage, and DOM
+document.getElementById('clear-btn').addEventListener("click", function(){
+    localStorage.clear();
+    myLeads = [];
+    renderLeads(myLeads);
+})
+
+document.getElementById('save-btn').addEventListener("click", function(){
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        renderLeads(myLeads);
+    })
+})
+
+//fetch data from input field text and pushes to array and 
+document.getElementById('input-btn').addEventListener("click", function(){
+    myLeads.push(input.value);
+    input.value = "";
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads(myLeads);
+})
+
